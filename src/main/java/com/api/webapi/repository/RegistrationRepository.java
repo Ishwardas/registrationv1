457d194767be2260cc40174e47d029e994f61cfb
Ishwardas <ishwardas39@gmail.com>
2024-10-01 11:46:52 +0530

    Delete src/main/java/com/api/webapi directory

--- src/main/java/com/api/webapi/repository/RegistrationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.api.webapi.repository;
-
-import com.api.webapi.entity.Registration;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegistrationRepository extends JpaRepository<Registration, Long> {
-}
